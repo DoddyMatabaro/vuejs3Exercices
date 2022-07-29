881a81e4b528b972a65620de99c5a5c91f2390bd
Doddy MATABARO <matabarododdy@gmail.com>
2022-07-29 15:24:24 +0200

    one and two container

---
+++ src/components/MainCard.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="main-container">
+       <div class="container-one">
+            <div class="profile">
+                <img src="../assets/profile.jpg" alt="" srcset="">
+                <h3>Doddy MATABARO</h3>
+                <p>Web Develloper</p>
+            </div>
+            <div class="socials-links">
+                <a href="http://">
+                    <div class="link">
+                    <img src="../assets/github.svg" alt="">
+                        <h2>GitHub</h2>
+                    </div>
+                     <div class="link">
+                    <img src="../assets/linkedin.svg" alt="">
+                        <h2>Linkedin</h2>
+                    </div> <div class="link">
+                        <img src="../assets/twitter.svg" alt="">
+                        <h2>Twitter</h2>
+                    </div> <div class="link">
+                    <img src="../assets/instagram.svg" alt="">
+                        <h2>Instagram</h2>
+                    </div> 
+                </a>
+            </div>
+       </div>
+       <div class="container-two">
+            <div class="about-me">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                 Dolorum consequuntur eius, earum possimus natus consectetur
+                  atque, non hic pariatur aut quisquam quasi! Odio similique 
+                  quos facilis aut, fugit quasi quaerat.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, illum.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Lorem ipsum dolor sit.
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+            <div class="technology-section">
+                    <h1>Technologies Studied</h1>
+                    <div class="logos">
+                            
+                    </div>
+            </div>
+       </div>
+    </div>
+</template>
+
+
+<script>
+
+</script>
+
+<style>
+    .main-contenaier{
+
+    }
+    img{
+        height: 30px;
+    }
+</style>
