8d4f71b65c4d025b2090dbf0850604349864d722
Doddy MATABARO <matabarododdy@gmail.com>
2022-07-30 09:32:25 +0200

    ProjectsCard component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,30 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
     <MainCard />
+    <ProjectData  />
 </template>
 
 <script>
 import MainCard from "./components/MainCard.vue"
+import ProjectData from "./components/ProjectsCard";
+
 export default {
   name: 'App',
   components: {
-    MainCard
+    MainCard,
+    ProjectData
 }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+   
 </style>
---
+++ src/components/ProjectsCard.vue
@@ -0,0 +1,23 @@
+<template>
+    {{projectData.projectsArray}}
+
+    <div v-for="(items, index) in projectData.projectsArray" v-bind:key="items">
+        <h2>{{projectData.projectsArray[index].Title}}</h2>
+    </div>
+</template>
+
+<script>
+    import jsonData from "/projects.json";
+ 
+    export default{
+        data() {
+            return {
+                projectData: jsonData,
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
